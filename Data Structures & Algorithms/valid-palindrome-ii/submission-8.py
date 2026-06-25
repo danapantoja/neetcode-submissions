6a8db6e724ea20a75cdff10152ef30a9ef1d4a74
danapantoja <151599675+danapantoja@users.noreply.github.com>
2026-06-24 22:15:48 -0500

    Add: valid-palindrome-ii - submission-8

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if len(s) <=2: 
+            return True        
+        count = 0
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if s[left] !=s[right]:
+                #check if after current left or right deletion we get a palindrome
+                return s[left + 1 : right + 1][::-1] == s[left + 1 : right + 1] or s[left: right][::-1] ==s[left: right]
+            left+=1
+            right-=1
+        
+        return count<=1
+        
